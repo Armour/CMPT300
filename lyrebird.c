b3f42a50a5ec032ba701e2725461dc316848fa5e
Armour <497052684@qq.com>
2015-10-09 18:10:23 -0700

    add fork support

--- lyrebird.c
+++ lyrebird.c
@@ -18,32 +18,66 @@
 
 #include "lyrebird.h"
 #include "memwatch.h"
+#include "time.h"
+//#include "string.h"
 
 int main(int argc, char *argv[]) {
-    printf("--beginning of program\n");
+    FILE *fp;
+    char *enc_txt;
+    char *dec_txt;
+    char *out_time;
+    time_t raw_time;
+    int i, count = 0;
 
-    int counter = 0;
-    pid_t pid = fork();
-
-    if (pid == 0) {
-        // child process
-        int i;
-        for (i = 0; i < 500; ++i) {
-            printf("child process: counter=%d\n", ++counter);
-        }
-    } else if (pid > 0) {
-        // parent process
-        int j;
-        for (j = 0; j < 500; ++j) {
-            printf("parent process: counter=%d\n", ++counter);
-        }
-    } else {
-        // fork failed
-        printf("fork() failed!\n");
+    if (argc != 2) {
+        printf("erorr!\n");
         return 1;
     }
 
-    printf("--end of program--\n");
+    fp = fopen(argv[1], "r+");
+    enc_txt = (char *)malloc(sizeof(char) * 1025);
+    dec_txt = (char *)malloc(sizeof(char) * 1025);
+    pid_t pid;
 
+    while (1) {
+        fscanf(fp, "%s", enc_txt);
+        fscanf(fp, "%s", dec_txt);
+        if (feof(fp)) {
+            free(enc_txt);
+            free(dec_txt);
+            fclose(fp);
+            break;
+        }
+
+        pid = fork();
+        time(&raw_time);
+        out_time = ctime(&raw_time);
+
+        if (pid < 0) {
+            // fork failed
+            printf("fork() failed!\n");
+            return 1;
+        }
+
+        if (pid != 0) {
+            count++;
+            printf("[%s] Child process ID #%d created to decrypt %s\n", out_time, (int)pid, enc_txt);
+        } else if (pid == 0) {
+            // child process
+            printf("in child process %d\n", getpid());
+            sleep(3);
+            printf("Done child %d\n", getpid());
+            free(enc_txt);
+            free(dec_txt);
+            return 0;
+        }
+    }
+
+    for(i = 0; i < count; i++) {
+        wait(NULL);
+        printf("Done %d\n", i);
+    }
+
+    printf("exit!\n");
     return 0;
 }
