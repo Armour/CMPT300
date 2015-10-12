9820dc99f0eb0a7c1178eb134eb5a58c79e35a63
Armour <497052684@qq.com>
2015-10-12 16:30:40 -0700

    finished a2!

--- lyrebird.c
+++ lyrebird.c
@@ -81,55 +81,67 @@ void clean_up(void) {               /* Always remember to free all and close fil
 
 int main(int argc, char *argv[]) {
     int i, count = 0;
+    int flag = 0;                   /* Used to store return value for main function */
+
+    out_time = (char *)malloc(sizeof(char) * TIME_MAXLENGTH);
 
     if (argc != 2) {                /* Check if the arguments number is right or not */
-        printf("Arguments number is not right! Usage: ./lyrebird config_file_name_here.txt\n");
+        get_time(out_time);
+        printf("[%s] (#%d) Arguments number is not right! Usage: ./lyrebird config_file_name_here.txt\n", out_time, getpid());
+        free(out_time);
         return 1;
     }
 
     fp = fopen(argv[1], "r+");      /* Input config file */
 
     if (!fp) {                      /* Check if the input file is exist or not */
-        printf("Error when opening file %s, input file not exist!\n", argv[1]);
+        get_time(out_time);
+        printf("[%s] (#%d) Error when opening file %s, input file not exist!\n", out_time, getpid(), argv[1]);
+        free(out_time);
         return 1;
     }
 
     enc_txt = (char *)malloc(sizeof(char) * FILE_MAXLENGTH);
     dec_txt = (char *)malloc(sizeof(char) * FILE_MAXLENGTH);
-    out_time = (char *)malloc(sizeof(char) * TIME_MAXLENGTH);
 
     while (fscanf(fp, "%s", enc_txt) != EOF) {      /* Read until end of file */
         fscanf(fp, "%s", dec_txt);
 
-        pid = fork();               /* Fork! */
+        pid = fork();                               /* Fork! */
 
-        if (pid < 0) {              /* If fork failed */
+        if (pid < 0) {                              /* If fork failed */
             get_time(out_time);
-            printf("[%s] Fork failed in process #%d!\n", out_time, getpid());
-            clean_up();             /* Always remember to free all and close file pointer! */
-            return 1;               /* Exit non-zero value */
+            printf("[%s] Fork failed in create new process #%d!\n", out_time, getpid());
+            clean_up();                             /* Always remember to free all and close file pointer! */
+            flag = 1;                               /* Exit non-zero value */
+            break;                                  /* Break and still need to wait for all child processes */
         }
 
-        if (pid != 0) {             /* If fork successful and is in parent process */
+        if (pid != 0) {                             /* If fork successful and is in parent process */
             count++;
             get_time(out_time);
             printf("[%s] Child process ID #%d created to decrypt %s\n", out_time, (int)pid, enc_txt);
-        } else if (pid == 0) {      /* Else if in child process */
-            int status = decrypt(enc_txt, dec_txt);
+        }
+
+        if (pid == 0) {                             /* If in child process */
+            int state = decrypt(enc_txt, dec_txt);
             get_time(out_time);
-            if (status == 0)        /* If child process exit without error */
+            if (state == 0)                         /* If child process exit without error */
                 printf("[%s] Decryption of %s complete. Process ID #%d Exiting.\n", out_time, enc_txt, getpid());
-            else                    /* If child process terminate unexpectly! */
-                printf("[%s] Child process ID #%d did not terminate successfully.\n", out_time, getpid());
-            clean_up();             /* Always remember to free all and close file pointer! */
-            return 0;
+            clean_up();                             /* Always remember to free all and close file pointer! */
+            exit(state);
         }
     }
 
-    for(i = 0; i < count; i++) {    /* Parent process wait for all child processes before exit */
-        wait(NULL);
+    for (i = 0; i < count; i++) {                   /* Parent process wait for all child processes before exit */
+        int state;
+        int pid = wait(&state);                     /* Wait until found one child process finished */
+        get_time(out_time);
+        if (state != 0) {                           /* If child process terminate unexpectly! */
+            printf("[%s] Child process ID #%d did not terminate successfully.\n", out_time, pid);
+        }
     }
 
-    clean_up();                     /* Always remember to free all and close file pointer! */
-    return 0;
+    clean_up();                                     /* Always remember to free all and close file pointer! */
+    return flag;
 }
